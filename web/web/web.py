468e25b7cedc5fc90679e35f83493c4b041dcb00
José de la Sierra <lasierrayelrio@gmail.com>
2024-06-27 17:21:39 +0200

    [Refactor] Refactor the entire code

---
+++ web/web/web.py
@@ -0,0 +1,11 @@
+import reflex as rx
+
+class State(rx.State):
+    pass
+
+def index() -> rx.Component:
+    return rx.text("Hola Reflex!", color ="blue")
+
+app = rx.App()
+app.add_page(index)
+app._compile()
