eaabcbb6c8fd3533eec4c432580f24e0a7da5f5b
José de la Sierra <lasierrayelrio@gmail.com>
2024-06-28 08:51:12 +0200

    [Feature] Add navbar component

--- web/web/web.py
+++ web/web/web.py
@@ -1,11 +1,20 @@
 import reflex as rx
+from web.components.navbar import navbar
 
 class State(rx.State):
     pass
 
 def index() -> rx.Component:
-    return rx.text("Hola Reflex!", color ="blue")
+    return rx.vstack(
+        navbar(),
+        
+    )
 
+
+#NavBar
+
+
+# App
 app = rx.App()
 app.add_page(index)
 app._compile()
